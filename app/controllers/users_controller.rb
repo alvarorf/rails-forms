8fc1c19e36ee38724be54770a00f3a4b53da84b5
alvaro <aaruizf@unal.edu.co>
2020-11-03 16:30:30 -0500

    Add functionality to create new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
     def create
+        puts "params is: #{params}"
+        puts "And params[:email] is: #{params[:email]}"
+        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        if @user.save
+            redirect_to(new_user_path)
+        else
+            render :new
+        end
     end
 
     def new
