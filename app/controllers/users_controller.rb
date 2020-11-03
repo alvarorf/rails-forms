29dd36afa36b6a2c5780d82a2f0e1cf3bbb107c4
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-11-03 23:52:36 +0200

    Use params_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
     def create
-        puts "params is: #{params}"
-        puts "And params[:email] is: #{params[:email]}"
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        #puts "params is: #{params}"
+        #puts "And params[:email] is: #{params[:email]}"
+        #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        @user = User.new(user_params)
         if @user.save
-            redirect_to(new_user_path)
+            # redirect_to new_user_path new_users_path
+            redirect_to '/users/new'
+
         else
             render :new
         end
@@ -12,4 +15,8 @@ class UsersController < ApplicationController
 
     def new
     end
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+      end
 end
