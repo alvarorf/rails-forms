abcc0f035f15049eb3f0fa4f1623376a3f58c31e
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-11-04 22:27:39 +0200

    Fix issues about the creation of an user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,24 +9,29 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sleep(2.5)
-      redirect_to edit_users_url, notice: "User '#{@user.username}' has been created!"
+      redirect_to new_users_path, notice: "User '#{@user.username}' has been created!"
     else
       render :new
     end
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       sleep(2.5)
-      redirect_to new_users_url, notice: "User '#{@user.username}' has been updated!"
+      redirect_to edit_user_path, notice: "User '#{@user.username}' has been updated!"
     else
       render :edit
     end
   end
 
-  def new; end
+  def new
+    @user = User.new
+  end
 
-  def edit; end
+  def edit
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
