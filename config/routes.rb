9de1ebc938b780909c43be419e772d330bde85d2
alvaro <aaruizf@unal.edu.co>
2020-11-04 18:37:36 -0500

    Modify routes and README.md

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'users#new'
-  resource :users, only: [:new, :create, :edit, :update]
-  #get 'users/:id/edit' => "users#edit"
+  resource :users, only: [:new, :create]
+  put "users/:id" => "users#update"
+  get "users/:id/edit" => "users#edit"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
