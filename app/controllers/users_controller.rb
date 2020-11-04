d544aab62ce37b4af8bada01db33a9956c9df114
alvaro <aaruizf@unal.edu.co>
2020-11-04 12:33:14 -0500

    Add form_tag implementation in new.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
+
+  def show; end
+
+  def index
+    @users = User.all
+  end
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to '/users/new'
+      flash[:notice] = "User '#{@user.username}' has been created!"
+      sleep(2.5)
+      redirect_to @user
     else
       render :new
     end
