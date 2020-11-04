58740a831fced87285ea9cba16c39d1f1851bf3a
alvaro <aaruizf@unal.edu.co>
2020-11-04 13:00:33 -0500

    Add helper functions in edit and new templates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
-
   def show; end
 
   def index
     @users = User.all
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "User '#{@user.username}' has been created!"
       sleep(2.5)
-      redirect_to @user
+      redirect_to edit_users_url
     else
       render :new
     end
@@ -20,12 +20,14 @@ class UsersController < ApplicationController
     if @user.update(user_params)
       flash[:notice] = "User '#{@user.username}' has been updated!"
       sleep(2.5)
-      redirect_to @user
+      redirect_to new_users_url
     else
       render :edit
     end
   end
+
   def new; end
+
   def edit; end
 
   def user_params
